e8e634c53c0ada3fe58c8ae01ed4c29091400b15
William Pride <william.pride@flexport.com>
2018-12-16 16:47:39 -0800

    Heroku stuff

--- server/boot/script.js
+++ server/boot/script.js
@@ -1,3 +1,14 @@
+'use strict';
+
+var path = require('path');
+
 module.exports = function(app, cb) {
+  var router = app.loopback.Router();
+  router.get('/status', app.loopback.status());
+  router.get('/', function(req, res) {
+    var indexFile = path.resolve(__dirname, '../..', app.get('indexFile'));
+    res.sendFile(indexFile);
+  });
+  app.use(router);
   app.start();
 };
--- server/server.js
+++ server/server.js
@@ -4,10 +4,15 @@ var loopback = require('loopback');
 var boot = require('loopback-boot');
 
 var app = module.exports = loopback();
+const path = require('path');
 
 app.start = function() {
   return app.listen(function() {
     app.emit('started');
+    var staticFolder = path.dirname(
+      path.resolve(__dirname, '..', app.get('indexFile'))
+    );
+    app.use(loopback.static(staticFolder));
     var baseUrl = app.get('url').replace(/\/$/, '');
     console.log('Web server listening at: %s', baseUrl);
     if (app.get('loopback-component-explorer')) {

74b1d8d501efbf2ba895508afa21b4959e75babe
William Pride <william.pride@flexport.com>
2018-12-30 12:05:29 -0500

    Save

--- client/src/NewSession.js
+++ client/src/NewSession.js
@@ -2,16 +2,14 @@ import React, { Component } from 'react';
 import { Button } from 'react-bootstrap';
 import moment from 'moment';
 
-class NewSession extends Component {
+class AddExerciseComponent extends Component {
   constructor(props) {
     super(props);
     this.state= {
       sets: [0],
-      exercises: [],
       exerciseId: null,
     };
-    this.handleSubmit = this.handleSubmit.bind(this);
-    this.handleChange = this.handleChange.bind(this);
+    this.handleWeightChange = this.handleWeightChange.bind(this);
     this.handleRepsChange = this.handleRepsChange.bind(this);
     this.handleAddSet = this.handleAddSet.bind(this);
     this.handleExerciseChange = this.handleExerciseChange.bind(this);
@@ -21,7 +19,7 @@ class NewSession extends Component {
     const {id, name} = exercise;
     return <option value={id}>{name}</option>;
   }
-  handleChange(event) {
+  handleWeightChange(event) {
     this.setState({weight: event.target.value});
   }
   handleExerciseChange(event) {
@@ -34,30 +32,8 @@ class NewSession extends Component {
     this.setState({sets: sets});
   }
 
-  handleSubmit(event) {
-    event.preventDefault();
-    fetch('http://localhost:3000/api/exerciseSets/newExerciseSet', {
-      method: 'POST',
-      headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        exerciseId: this.state.exerciseId,
-        weight: this.state.weight,
-        sets: this.state.sets,
-      })
-    })
-  }
-
   handleAddSet() {
     this.setState({sets: this.state.sets.concat(0)});
-  }
-  componentWillMount() {
-    fetch('http://localhost:3000/api/Exercises')
-      .then(response => response.text())
-      .then(JSON.parse)
-      .then(exercises => this.setState({ exercises }));
   }
 
   getRepsInput(set, index) {
@@ -84,28 +60,103 @@ class NewSession extends Component {
   }
 
   render() {
-    const {exercises, sets} = this.state;
-    console.log(exercises);
+    const {sets} = this.state;
+    const {exercises} = this.props;
     return (
-      <form onSubmit={this.handleSubmit}>
-        <label>
-          Exercise:
-          <select value={this.state.exerciseId} onChange={this.handleExerciseChange}>
-            {exercises.map((exercise) => this.getExerciseOption(exercise))}
-          </select>
-        </label>
-        <label>
-          Weight:
-          {this.getWeightInput()}
-        </label>
-        <label>
-          Reps:
-          {sets.map((set, index) => this.getRepsInput(set, index))}
-        </label>
-        <Button onClick={() => this.handleAddSet()}>Add Set</Button>
-        <input type="submit" value="Submit" />
-      </form>
-  )};  
+    <>
+      <label>
+        Exercise:
+        <select value={this.state.exerciseId} onChange={this.handleExerciseChange}>
+          {exercises.map((exercise) => this.getExerciseOption(exercise))}
+        </select>
+      </label>
+      <label>
+        Weight:
+        {this.getWeightInput()}
+      </label>
+      <label>
+        Reps:
+        {sets.map((set, index) => this.getRepsInput(set, index))}
+      </label>
+      <Button onClick={() => this.handleAddSet()}>Add Set</Button>
+    </>
+    )
+  };  
+
+}
+
+class NewSession extends Component {
+  constructor(props) {
+    super(props);
+    this.state= {
+      date: new Date(),
+      exercises: [],
+      exerciseSetCount: 1,
+    };
+    this.handleAddExerciseSet = this.handleAddExerciseSet.bind(this);
+    this.handleDateChange = this.handleDateChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    fetch('http://localhost:3000/api/exerciseSets/newExerciseSet', {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        date: this.state.date,
+        exerciseId: this.state.exerciseId,
+        weight: this.state.weight,
+        sets: this.state.sets,
+      })
+    })
+  }
+  handleDateChange(event) {
+    this.setState({date: event.target.value});
+  }
+  handleAddExerciseSet() {
+    this.setState({exerciseSetCount: this.state.exerciseSetCount + 1});
+  }
+  componentWillMount() {
+    fetch('http://localhost:3000/api/Exercises')
+      .then(response => response.text())
+      .then(JSON.parse)
+      .then(exercises => this.setState({ exercises }));
+  }
+
+  render() {
+    const {date, exercises, exerciseSetCount} = this.state;
+    const exerciseComponents = [];
+    for (var i=0; i<exerciseSetCount; i++) {
+      exerciseComponents.push(
+        <>
+        <AddExerciseComponent exercises={exercises} />
+        <br/>
+        </>
+      )
+    }
+    return (
+      <>
+        <form onSubmit={this.handleSubmit}>
+          <input 
+            type="date" 
+            id="date" 
+            name="date"
+            onChange={(event) => this.handleDateChange(event)}
+            value={date} />
+          <br/><br/>
+          {exerciseComponents}
+          <br/>
+          <Button onClick={() => this.handleAddExerciseSet()}>Add Exercise</Button>
+          <br/>
+          <input type="submit" value="Submit" />
+        </form>
+      </>
+    )
+  };
 }
 
 export default NewSession;
